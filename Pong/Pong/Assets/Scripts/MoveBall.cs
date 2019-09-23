3da966440d9b6d9644081754ac7657785cca35d1
David Cruz <d.crux10@gmail.com>
2019-09-23 14:56:04 -0600

    Added MoveBall script and Brain for the ANN

--- Pong/Pong/Assets/Scripts/MoveBall.cs
+++ Pong/Pong/Assets/Scripts/MoveBall.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class MoveBall : MonoBehaviour {
 
+    Vector3 ballStartPosition;
+    Rigidbody2D rb;
+    float speed = 400;
+    public AudioSource blip;
+    public AudioSource blop;
+
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody2D> ();
+        ballStartPosition = transform.position;
+        ResetBall ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void Update () {
+        if (Input.GetKeyDown("space")) {
+            ResetBall ();
+        }
+    }
+
+    private void OnCollisionEnter2D ( Collision2D collision ) {
+        if (collision.gameObject.tag == "backwall") {
+            blop.Play ();
+        } else {
+            blip.Play ();
+        }    
+    }
+
+    void ResetBall () {
+        transform.position = ballStartPosition;
+        rb.velocity = Vector3.zero;
+        Vector3 dir = new Vector3 (Random.Range(100,300), Random.Range(-100,100),0).normalized;
+        rb.AddForce (dir*speed);
+    }
 }
