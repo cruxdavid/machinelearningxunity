ae0b4c02378de1e70c835754b8f3d86294bf6096
David Cruz <d.crux10@gmail.com>
2019-09-26 18:18:17 -0600

    Created QNetwork project

---
+++ QNetworks/QNetworks/Assets/Scripts/Brain.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Brain : MonoBehaviour {
+    
+}
+
+public class Replay {
+
+    public List<double> states;
+    public double reward;
+
+    public Replay ( double xr , double ballz , double ballvx , double r ) {
+        states = new List<double> ();
+        states.Add ( xr );
+        states.Add ( ballz );
+        states.Add ( ballvx );
+        reward = r;
+
+    }
+
+}
